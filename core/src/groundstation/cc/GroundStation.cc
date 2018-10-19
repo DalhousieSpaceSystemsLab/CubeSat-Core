e241667f92fafb93a0a14a1e1bae6f44c5df2a5e
Kyle Hall <kpwh23@mun.ca>
2018-10-19 18:37:02 -0230

    Added GroundStation with basic functionality. Modified: 	CubeSat.h/cc :	Modified to correspond with new UML design I have 			been given access to.
    Added:
    	GroundStation.h/cc : Implemented ground station with basic functionality.
    
    TODO:
    	- GroundStation::ProcessMessage() remains unimplemented.
    	- GroundStation and CubeSat are inconsistent in there usage of char[] and Message.
    	- Naming conventions have been used inconsistently.

---
+++ core/src/groundstation/cc/GroundStation.cc
@@ -0,0 +1,13 @@
+#include "GroundStation.h"
+
+// Null constructor for GroundStation class
+//
+GroundStation::GroundStation() {
+    // Null
+}
+
+// Constructor for GroundStation class
+//
+GroundStation::GroundStation(Terminal terminal) {
+    this->terminal_=terminal;
+}
