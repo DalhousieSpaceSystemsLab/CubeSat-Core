0102fc8a0d73f03bc1bf05912ee245d14933976f
AndrewWay <arw405@mun.ca>
2018-11-06 18:51:31 -0330

    Terminal TODOs

--- core/src/terminal/cc/Terminal.cc
+++ core/src/terminal/cc/Terminal.cc
@@ -1,23 +1,28 @@
 #include "../header/Terminal.h";
 
 void Terminal::chat() {
-
+    //TODO Create a while loop
+    //TODO In the while loop, display a list of options: 1. List Commands 2. Send command 3. Exit
+    //TODO Have if statements to handle user input and call the appropriate functions
+    
 }
 
 void Terminal::sendCommand(char *command) {
-
+    //TODO Call this terminal's groundstation's "SendMessageToCubeSat" function. Pass the command argument to that function.
 }
 
 void Terminal::listCommands() {
-
+    //TODO Get the commands loaded in _commands and print them
 }
 
 void Terminal::displayMessage(char *message) {
-
+    //TODO Print message
 }
 
 void Terminal::loadCommands() {
-
+    //TODO Call TerminalCommandLoader::readCommands
+    
+    //TODO Set commands_ to the returned array 
 }
 
 void Terminal::set_ground_station(GroundStation ground_station) {
