afa728ec0e8a19fbd89281a3231ee7db29b7601c
AndrewWay <arw405@mun.ca>
2018-11-18 14:45:12 -0330

    TODOs
    TerminalCommand

--- core/src/terminal/cc/TerminalCommand.cc
+++ core/src/terminal/cc/TerminalCommand.cc
@@ -9,14 +9,17 @@ TerminalCommand::TerminalCommand(std::string name, std::string description) {
     this->description_ = description;
 }
 
+//TODO refactor. Change getName to name
 std::string TerminalCommand::getName() {
     return this->name_;
 }
 
+//TODO refactor. Change getDescription to description
 std::string TerminalCommand::getDescription() {
     return this->description_;
 }
 
+//TODO refactor. Change toString to ToString
 std::string TerminalCommand::toString() {
     return name_ + "," + description_;
 }
