4e88989e33325a062621a8180f650002b544eefe
Michael <mc563455@dal.ca>
2019-03-15 14:55:36 -0300

    Struct Serializer and Message Struct
    Struct 'Serializer' built as 3 functions: Serialize,  'Deserialize' and 'PacketSize' which returns the total amount of data to be serialized.

---
+++ core/src/tcp/Serialize/Struct.h
@@ -0,0 +1,17 @@
+#ifndef STRUCT
+#define STRUCT
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef struct Message
+{
+	string type;
+	
+	string contents;
+	
+}Message;
+
+#endif
+
