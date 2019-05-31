313eade3dee04e6129106195b8833ebb835eaf02
Michael <mc563455@dal.ca>
2019-05-31 15:13:50 -0300

    added getters' and setters, cleaned up serialization

--- core/src/message/header/MessageHeader.h
+++ core/src/message/header/MessageHeader.h
@@ -1,16 +1,19 @@
 
 #ifndef LORIS_MESSAGE_MESSAGEHEADER_H_
 #define LORIS_MESSAGE_MESSAGEHEADER_H_
-#include "Message.h"
 
 class MessageHeader {
 public:
     MessageHeader(unsigned int recipient, unsigned int sender, long time_created);
     
-    friend SerializeMessage(Message *message, char *data);
-    
-	friend Message DeserializeMessage(char *data);
+    unsigned int GetSender() const;
+	unsigned int GetRecipient() const;
+	long GetTimeCreated() const;
 
+	void SetSender(unsigned int sender_);
+	void SetRecipient(unsigned int recipient_);
+	void SetTimeCreated(long time_created_);
+	
 private:
     unsigned int sender_;
     unsigned int recipient_;
