39c0d0b36662471b1b9341c9fef8846cd3c8f425
Andrew Way <arw405@mun.ca>
2019-06-20 15:36:22 -0230

    Added scripts for building LORIS Message library

--- emulation/src/main.cc
+++ emulation/src/main.cc
@@ -1,11 +1,12 @@
 #include "HardwareEmulationManager.h"
 #include <iostream>
 #include "PowerEmulator.h"
-
+#include "Message.h"
 
 int main() {
   //HardwareEmulationManager emulation_manager;
   //emulation_manager.Run(); 
   PowerEmulator emulator;
+  Message msg(0,0);
 
 }
