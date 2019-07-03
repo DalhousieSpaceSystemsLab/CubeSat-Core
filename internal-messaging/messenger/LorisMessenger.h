f098f4f77d61e921396258cc256c44f4e78117ed
Andrew Way <arw405@mun.ca>
2019-07-03 12:52:22 -0230

    Added new Messaging service directory in internal-messaging library

--- internal-messaging/messenger/LorisMessenger.h
+++ internal-messaging/messenger/LorisMessenger.h
@@ -4,12 +4,12 @@
  *  Created on: Jul 2, 2019
  *      Author: andrew
  */
-
-#include "Message.h"
-#include "MessageSenderInterface.h"
-
 #ifndef MESSENGER_LORISMESSENGER_H_
 #define MESSENGER_LORISMESSENGER_H_
+
+#include "Message.h"
+#include "MessageSenderInterface.h"//TODO LorisMessenger include MessagingService. Make MessagingService implement the interface, not inherit from it.
+
 class LorisMessenger{
 public:
 	LorisMessenger();
