357f3bb93764d89daa8b66964b91e220fe0a72ec
Andrew Way <arw405@mun.ca>
2019-08-02 11:02:06 -0230

    Implemented AddData function in Repository

--- core/src/repositories/cc/Repository.cc
+++ core/src/repositories/cc/Repository.cc
@@ -15,12 +15,13 @@ int Repository::HandleMessage(char *buffer){
     return 0;
 }
 
+//Check if the key is contained in the watch list for the repository.
 bool Repository::WatchListContainsKey(unsigned int key){
 
 	//TODO there is the potential for optimizing this search by
 	//sorting the array and using a search algorithm
 	for(int i=0;i<this->watch_list_.size();i++){
-		if(watch_list_.at(i)==key){
+		if(watch_list_.at(i).key()==key){
 			return true;
 		}
 	}
@@ -33,13 +34,19 @@ int Repository::AddData(DataMessage message){
 	std::vector<int> int_keys = message.GetIntKeys();
 
 	for(int i=0;i<float_keys.size();i++){
-		if(WatchListContainsKey(float_keys[i])){
-
+		unsigned int current_key = float_keys[i];
+		if(WatchListContainsKey(current_key)){
+			repository_data_.AddKeyValuePair(current_key,message.
+					GetFloat(current_key));
 		}
 	}
 
 	for(int i=0;i<int_keys.size();i++){
-
+		unsigned int current_key = int_keys[i];
+		if(WatchListContainsKey(current_key)){
+			repository_data_.AddKeyValuePair(current_key,message.
+					GetInt(current_key));
+		}
 	}
 
 
--- core/src/repositories/header/Repository.h
+++ core/src/repositories/header/Repository.h
@@ -22,6 +22,11 @@ protected:
 private:
     //Keys will be added to the watch_list_ in this function. Must be implemented or else
     //the repository will never accept, store, or return data.
+    /*
+     * The idea behind a watch list is that the repository will only ever accept incoming data
+     * which are listed in the watch list. For example, the Power repository will only
+     * accept data which has a key listed in its watch list.
+     */
     virtual int AddKeysToWatchList()=0;
     //All data (key value pairs) are stored in here.
     //Only key value pairs with keys in the watch list should be added to it.
