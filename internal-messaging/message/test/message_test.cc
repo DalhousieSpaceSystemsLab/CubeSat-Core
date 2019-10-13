4c4dd64205add8c61ed172c01c242b85ed58613d
Richard Walmsley <r.walmsley97@gmail.com>
2019-10-13 00:57:44 -0230

    Added bones for tests using gtest

--- internal-messaging/message/test/message_test.cc
+++ internal-messaging/message/test/message_test.cc
@@ -5,42 +5,53 @@ using std::endl;
 #include "DataMessage.h"
 #include <gtest/gtest.h>
 
-int main(){
-cout << "Hello! This is a message creation test!" << endl;
 //Empty message test
 DataMessage message = DataMessage();
-int capacity=128;
+const int capacity = 128;
 char str[capacity];
-message.ToString(str,capacity);
-for(int i=0;i<capacity;i++){
-  cout << str[i];
+
+TEST(messageTest, emptyMessage){
+  message.ToString(str,capacity);
+
+  ASSERT_EQ(NULL, str);
 }
-cout << endl;
 
-//Message with int key value pair test
-memset(str, 0, sizeof(str));
-message.Add(0,0);
-message.Add(1235,9393);
-message.Add(10,-7000);
-message.ToString(str,capacity);
-for(int i=0;i<128;i++){
-  cout << str[i];
+TEST(messageTest, intKeyValuePair){
+  memset(str, 0, sizeof(str));
+  message.Add(0, 0);
+  message.Add(1235, 9393);
+  message.Add(10, -7000);
+  message.ToString(str,capacity);
+
+  for (int i = 0; i < 128; i++){
+    cout << str[i];
+  }
+  cout << endl;
+
+  ASSERT_EQ(0, str[0]);
+  ASSERT_EQ(0, str[1]);
+  ASSERT_EQ(1235, str[2]);
+  ASSERT_EQ(9393, str[3]);
+  ASSERT_EQ(10, str[4]);
+  ASSERT_EQ(-7000, str[5]);
 }
-cout << endl;
 
-//Message with one int key value pair and float pair test
-memset(str, 0, sizeof(str));
-message.Add(1,12497.2f);
-message.Add(1923,94.2f);
-message.Add(123,0.8f);
-message.Add(85,-1.0f);
-
-message.ToString(str,capacity);
-for(int i=0;i<128;i++){
-  cout << str[i];
+TEST(messageTest, intKeyValueAndFloatPair){
+  memset(str, 0, sizeof(str));
+  message.Add(1, 12497.2f);
+  message.Add(1923, 94.2f);
+  message.Add(123, 0.8f);
+  message.Add(85, -1.0f);
+  message.ToString(str,capacity);
 }
-cout << endl;
 
+TEST(messageTest, bufferOverflow){
+  memset(str, 0, sizeof(str));
+  message.ToString(str,capacity);
+}
 
-return 0;
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
 }
