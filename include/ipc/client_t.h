de70b93a2c9b9e28f1c3712fd9f0912bdb7711a5
Alex <git@alexamellal.com>
2020-06-25 20:18:18 -0300

    added standardized functions to client_t

--- include/ipc/client_t.h
+++ include/ipc/client_t.h
@@ -12,10 +12,15 @@
 // Connection type
 #include "conn_t.h"
 
+// Client type
 typedef struct Client
 {
   conn_t  conn;     // connection socket placeholders
   char    name[3];  // client name placeholder
 } client_t;
+
+// Standardized methods for the client type 
+client_t  client_t_new();                     // Returns initialized client_t
+int       client_t_close(client_t * client);  // Closes conn member of client_t
 
 #endif /* end of include guard: CUBESAT_CORE_INCLUDE_IPC_CLIENT_T_H */
