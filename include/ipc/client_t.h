23e2e76cb9722ee1e456ea96d48161bdaed7e6e9
Alex <git@alexamellal.com>
2020-06-26 09:23:26 -0300

    added client_t stat function

--- include/ipc/client_t.h
+++ include/ipc/client_t.h
@@ -12,6 +12,9 @@
 // Connection type
 #include "conn_t.h"
 
+// Standard C libraries
+#include <string.h>
+
 // Client type
 typedef struct Client
 {
@@ -21,6 +24,7 @@ typedef struct Client
 
 // Standardized methods for the client type 
 client_t  client_t_new();                     // Returns initialized client_t
+int       client_t_stat(client_t client);     // Returns 0 if client in use, -1 if vacant
 int       client_t_close(client_t * client);  // Closes conn member of client_t
 
 #endif /* end of include guard: CUBESAT_CORE_INCLUDE_IPC_CLIENT_T_H */
