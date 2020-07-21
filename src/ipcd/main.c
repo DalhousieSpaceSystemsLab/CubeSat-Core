6bcf6b797b6c786f6e1b00490bb346fb7f1ff3f1
amellalalex <github@alexamellal.com>
2020-07-21 18:09:31 -0300

    removed excessive function calls for ipc client

--- src/ipcd/main.c
+++ src/ipcd/main.c
@@ -21,28 +21,28 @@ int main()
     return -1;
   }
 
-  // Start accepting new clients 
-  if(ipcd_start_accepting() == -1) // ipcd_start_accepting() failed 
-  {
-    fprintf(stderr, "ipcd_start_accepting() failed\n");
-    return -1;
-  }
+  // // Start accepting new clients 
+  // if(ipcd_start_accepting() == -1) // ipcd_start_accepting() failed 
+  // {
+  //   fprintf(stderr, "ipcd_start_accepting() failed\n");
+  //   return -1;
+  // }
 
-  // Start routing messages between clients 
-  if(ipcd_start_routing() == -1) // ipcd_start_routing() failed 
-  {
-    fprintf(stderr, "ipcd_start_routing() failed");
-    return -1;
-  }
+  // // Start routing messages between clients 
+  // if(ipcd_start_routing() == -1) // ipcd_start_routing() failed 
+  // {
+  //   fprintf(stderr, "ipcd_start_routing() failed");
+  //   return -1;
+  // }
 
-  for(;;)
-  {
-    ipcd_print_clients();
+  // for(;;)
+  // {
+  //   ipcd_print_clients();
 
-    printf("Press [ENTER] to print again or 'q' to stop printing.\n");
-    char input = getc(stdin);
-    if(input == 'q') break;  
-  }
+  //   printf("Press [ENTER] to print again or 'q' to stop printing.\n");
+  //   char input = getc(stdin);
+  //   if(input == 'q') break;  
+  // }
 
   // Wait for user to press enter 
   printf("Press [ENTER] to quit\n");
