6d8c4ec6b584b4f916ed23eb6c3ab72daaf15195
amellalalex <github@alexamellal.com>
2020-08-07 18:41:23 -0300

    added test_client_api_disconnect test

--- src/tests/test.c
+++ src/tests/test.c
@@ -6,6 +6,8 @@ int main()
     cmocka_unit_test(test_client_api_connect),
     cmocka_unit_test(test_client_api_send),
     cmocka_unit_test(test_client_api_recv),
+    cmocka_unit_test(test_client_api_refresh),
+    cmocka_unit_test(test_client_api_disconnect),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
