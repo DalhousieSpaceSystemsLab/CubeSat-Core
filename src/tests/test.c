c26fb2900ea2e01189d7243a483a71aba2e08827
amellalalex <github@alexamellal.com>
2020-08-07 18:17:09 -0300

    added test_client_api_recv test

--- src/tests/test.c
+++ src/tests/test.c
@@ -5,6 +5,7 @@ int main()
   const struct CMUnitTest tests[] = {
     cmocka_unit_test(test_client_api_connect),
     cmocka_unit_test(test_client_api_send),
+    cmocka_unit_test(test_client_api_recv),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
