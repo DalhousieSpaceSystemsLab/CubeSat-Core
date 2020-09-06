20d90c811f1699a1e493799aa9a05fba4b5be87e
amellalalex <github@alexamellal.com>
2020-09-06 17:58:45 -0300

    reformatted server_container struct

---
+++ include/subsystem/server_container.h
@@ -0,0 +1,26 @@
+/**
+* server_container.h
+*
+*   purpose: provide a modular placeholder for subsystem servers (container)
+*   author: alex amellal
+*
+*/
+
+#ifndef CUBESAT_CORE_INCLUDE_SUBSYSTEM_SERVER_H
+#define CUBESAT_CORE_INCLUDE_SUBSYSTEM_SERVER_H
+
+// Project headers
+#include "subsystem/settings.h"
+
+// Standard C libraries
+#include <pthread.h>
+#include <stddef.h>
+
+// Subsystem server container type
+typedef struct server_container {
+  void* (*start)();
+  void (*stop)();
+  pthread_t tid;
+} ServerContainer;
+
+#endif  // END OF CUBESAT_CORE_INCLUDE_SUBSYSTEM_SERVER_H
