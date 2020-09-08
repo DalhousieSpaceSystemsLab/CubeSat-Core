1f90ba2e18217936af1326cf615dc194fee85619
amellalalex <github@alexamellal.com>
2020-09-08 18:54:08 -0300

    created filesystem server (primitive)

---
+++ include/subsystem/servers/filesystem.h
@@ -0,0 +1,19 @@
+/**
+ * filesystem.h 
+ * 
+ * purpose: Stores, retrieves and organizes files on the 
+ *          satellite's filesystem.
+ * author:  alex amellal 
+ * 
+ */
+
+#ifndef CUBESAT_CORE_INCLUDE_SUBSYSTEM_SERVERS_FILESYSTEM_H
+#define CUBESAT_CORE_INCLUDE_SUBSYSTEM_SERVERS_FILESYSTEM_H
+
+// Project headers
+#include "subsystem/server_container.h"
+#include "ipc/client_api.h"
+
+extern ServerContainer filesystem_server;
+
+#endif 
