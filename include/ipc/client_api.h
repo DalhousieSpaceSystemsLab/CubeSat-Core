6002afedbbdc5759a2c26b43645f769866e7f896
amellalalex <github@alexamellal.com>
2020-09-11 20:02:35 -0300

    begun C++ wrapper for ipc code

--- include/ipc/client_api.h
+++ include/ipc/client_api.h
@@ -24,14 +24,6 @@
 #include <errno.h>
 #include <time.h>
 
-////////////////
-//  C Version //
-////////////////
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 // Client API
 int ipc_connect(char name[3]);                                // Initialize client side IPC interface
 int ipc_send(char dest[3], char * msg, size_t msg_len);       // Send message to another process
@@ -40,45 +32,5 @@ int ipc_qsend(char dest[3], char * msg, size_t msg_len);      // Adds outgoing m
 int ipc_qrecv(char src[3], char * buf, size_t buf_len);       // Adds incoming message request to recv queue
 int ipc_refresh();                                            // Simultaneously reads/writes queued data
 int ipc_disconnect();                                         // Close client side IPC interface
-
-#ifdef __cplusplus
-}
-#endif
-
-//////////////////
-//  C++ Version //
-//////////////////
-
-#ifdef __cplusplus
-
-// Standard C++ Libraries
-#include <map>
-#include <iterator>
-#include <string>
-#include <exception>
-
-// Standard C++ classes
-using std::map;
-using std::pair;
-using std::string;
-using std::exception;
-
-// Client API (C++)
-namespace dss
-{
-  namespace ipc
-  {
-    // Methods
-    void    init(string name);
-    void    send(string dest, string msg);
-    void    send_map(char dest[3], map<string, string> * map);
-    void    send_map(char dest[3], map<string, int> * map);
-    void    send_map(char dest[3], map<int, int> * map);
-    string  recv(string src);
-    void    close();
-  };
-};
-
-#endif
 
 #endif /* end of include guard: CUBESAT_CORE_INCLUDE_IPC_CLIENT_API_H */
---
+++ src/ipc_client/client.cc
@@ -0,0 +1,29 @@
+/**
+ * client.cc
+ * 
+ * purpose: C++ wrapper for client.c 
+ * author:  alex amellal 
+ * 
+ * TODO: 
+ *  - Make C++ example ipc client
+ */
+
+// Project headers 
+#include "ipc/client_api.hpp"
+
+// Standard C++ libraries
+
+// Project namespace 
+using namespace dss;
+
+int main(int argc, char * argv[]) {
+  // Check number of args 
+  if(argc != 3) {
+
+  }
+  
+  // Connect to the IPC 
+
+
+  return 0;
+}
