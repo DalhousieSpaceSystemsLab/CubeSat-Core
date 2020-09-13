740003853715e69db1e24d4382d10ecb7b971044
amellalalex <github@alexamellal.com>
2020-09-13 09:34:47 -0300

    removed unused setting entry

--- include/ipc/settings.h
+++ include/ipc/settings.h
@@ -13,7 +13,6 @@
 #define MAX_NUM_CLI        5
 #define MAX_MSG_LEN        128
 #define NAME_LEN           3
-// #define READ_BLOCK_DELAY   1   // in seconds
 #define READ_BLOCK_DELAY (struct timespec) { .tv_sec = 0, .tv_nsec = 100000000 }
 #define ROUTER_CHECK_DELAY 1000000 // in nanoseconds
 #define DISCONNECT_SIG     "DISCONNECT"
