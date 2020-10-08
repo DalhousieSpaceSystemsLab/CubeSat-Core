5b98eda5dafaecb42b536e754e8db1d1a49c54cc
= <=>
2020-10-08 19:31:52 -0300

    added descriptions for every function and added a couple of functions

--- include/ipc/client_api.h
+++ include/ipc/client_api.h
@@ -10,9 +10,10 @@
 #define CUBESAT_CORE_INCLUDE_IPC_CLIENT_API_H
 
 // Project headers
-#include "settings.h"
-#include "util/immut.h"
+#include "ipc/settings.h"
 #include "ipc/client_t.h"
+#include "ipc/msg_req_dib.h"
+#include "util/immut.h"
 
 // Standard C Libraries
 #include <stdio.h>
@@ -23,14 +24,16 @@
 #include <pthread.h>
 #include <errno.h>
 #include <time.h>
+#include <stdbool.h>
 
 // Client API
-int ipc_connect(char name[3]);                                // Initialize client side IPC interface
-int ipc_send(char dest[3], char * msg, size_t msg_len);       // Send message to another process
-int ipc_recv(char src[3], char * buffer, size_t buffer_len);  // Receive message from another process (blocks)
-int ipc_qsend(char dest[3], char * msg, size_t msg_len);      // Adds outgoing message to send queue
-int ipc_qrecv(char src[3], char * buf, size_t buf_len);       // Adds incoming message request to recv queue
-int ipc_refresh();                                            // Simultaneously reads/writes queued data
-int ipc_disconnect();                                         // Close client side IPC interface
+int ipc_connect(char name[3]);                                                            // Initialize client side IPC interface
+int ipc_send(char dest[3], char * msg, size_t msg_len);                                   // Send message to another process
+int ipc_recv(char src[3], char * buffer, size_t buffer_len);                              // Receive message from another process (blocks)
+int ipc_qsend(char dest[3], char * msg, size_t msg_len);                                  // Adds outgoing message to send queue
+int ipc_qrecv(char src[3], char * buf, size_t buf_len, void (*callback)(char*, size_t));  // Adds incoming message request to recv queue
+int ipc_refresh();                                                                        // Simultaneously reads/writes all queued data
+int ipc_refresh_src(char src[NAME_LEN]);                                                  // Simultaneously reads/writes queued data for specific source 
+int ipc_disconnect();                                                                     // Close client side IPC interface
 
 #endif /* end of include guard: CUBESAT_CORE_INCLUDE_IPC_CLIENT_API_H */
