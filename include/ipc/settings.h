fffd72fd930f870ac4ddd72ef3844165bc107fdd
= <=>
2020-10-08 19:33:10 -0300

    added settings for dibs and updated max_num_cli

--- include/ipc/settings.h
+++ include/ipc/settings.h
@@ -10,13 +10,15 @@
 #define CUBESAT_CORE_INCLUDE_IPC_SETTINGS_H
 
 // IPC
-#define MAX_NUM_CLI        5
-#define MAX_MSG_LEN        128
-#define NAME_LEN           3
-#define READ_BLOCK_DELAY (struct timespec) { .tv_sec = 0, .tv_nsec = 100000000 }
-#define ROUTER_CHECK_DELAY 1000000 // in nanoseconds
-#define DISCONNECT_SIG     "DISCONNECT"
-#define RECV_CONF          "OK"
-#define RECV_TIMEOUT (struct timespec) { .tv_sec = 3, .tv_nsec = 0 } 
+#define MAX_NUM_CLI         16
+#define MAX_MSG_LEN         128
+#define NAME_LEN            3
+#define READ_BLOCK_DELAY    (struct timespec) { .tv_sec = 0, .tv_nsec = 100000000 }
+#define ROUTER_CHECK_DELAY  1000000 // in nanoseconds
+#define DISCONNECT_SIG      "DISCONNECT"
+#define RECV_CONF           "OK"
+#define RECV_TIMEOUT        (struct timespec) { .tv_sec = 3, .tv_nsec = 0 } 
+#define MAX_NUM_DIBS        MAX_NUM_CLI
+#define REFEED_DELAY        READ_BLOCK_DELAY
 
 #endif /* end of include guard: CUBESAT_CORE_INCLUDE_IPC_SETTINGS_H */
