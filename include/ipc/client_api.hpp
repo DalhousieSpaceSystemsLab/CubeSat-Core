605f88cd1582e69b61922ce868ff5295ceb2366a
= <=>
2020-10-08 19:32:18 -0300

    updated c++ client api to new ipc methods and organized nicely

--- include/ipc/client_api.hpp
+++ include/ipc/client_api.hpp
@@ -25,9 +25,17 @@ using std::endl;
 // Client API 
 namespace dss {
   namespace ipc {
-    void connect(string name);          // Initialize client side IPC interface
-    void send(string dest, string msg); // Send message to another process
-    string recv(string src);            // Receive message from another process (blocks)
-    void disconnect();                  // Close client side IPC interface
+    void connect(string name);            // Initialize client side IPC interface
+    void send(string dest, string msg);   // Send message to another process
+    string recv(string src);              // Receive message from another process (blocks)
+    void disconnect();                    // Close client side IPC interface
+
+    namespace async {
+      void send(string dest, string msg); // Adds outgoing message to send queue
+      void recv(string src);              // Adds message request to recv queue (non-blocking)
+      void recv(string src, void (*callback)(string)); // Same as recv but with callback
+      void refresh();                     // Simultaneously reads/writes all queued data and runs all callbacks
+      string refresh(string src);         // Refreshes specific source-filtered queue and returns result.
+    }
   }
 }
