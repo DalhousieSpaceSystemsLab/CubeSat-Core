289a64a7824e79a208ac02cfafab71693817e3da
= <=>
2020-10-08 19:32:37 -0300

    added a couple of c++ exceptions

--- include/ipc/exceptions.hpp
+++ include/ipc/exceptions.hpp
@@ -16,13 +16,25 @@ namespace dss {
   namespace ipc {
     class EInvalidName : public exception {
       const char * what() const throw() {
-        return "Invalid name provided to connect to the IPC. Must be a 3 letter name.";
+        return "Invalid name provided to connect to the IPC. Must be NAME_LEN characters long.";
       }
     };
 
     class EConn : public exception {
       const char * what() const throw() {
         return "Error encountered with connection to the IPC server (daemon)";
+      }
+    };
+
+    class EInvalidMsg : public exception {
+      const char * what() const throw() {
+        return "Provided message is incompatible with IPC standards. Cannot exceed MAX_MSG_LEN characters.";
+      }
+    };
+
+    class EAPI : public exception {
+      const char * what() const throw() {
+        return "Error encountered in underlying C api. Pay attention to other error messages to diagnose.";
       }
     };
   }
