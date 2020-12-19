bd5d647582d49d74dab270217369760f99f77910
amellalalex <github@alexamellal.com>
2020-12-19 12:38:45 -0400

    updated MsgReqDib to contain custom data pointer

--- include/ipc/msg_req_dib.h
+++ include/ipc/msg_req_dib.h
@@ -21,14 +21,15 @@
 // Message Request 'Dib' type 
 typedef struct msg_req_dib {
   char name[NAME_LEN];
-  void (*callback)(char*, size_t);
+  void (*callback)(char*, size_t, void*);
+  void* data;
 } MsgReqDib;
 
 // Standardized functions for message request dib 
-MsgReqDib MsgReqDib_new();                                                          // Returns blank dib 
-MsgReqDib MsgReqDib_set(char name[NAME_LEN], void (*callback)(char*, size_t));      // Returns initialized dib 
-int       MsgReqDib_add(MsgReqDib element, MsgReqDib * array, size_t array_len);    // Appends or overwrites dib into dib array
-bool      MsgReqDib_exists(char name[NAME_LEN], MsgReqDib *array, size_t array_len); // Checks in array for preexisting dibs
-int       MsgReqDib_remove(char name[NAME_LEN], MsgReqDib *array, size_t array_len); // Removes a dib from an array of dibs
+MsgReqDib MsgReqDib_new();                                                                        // Returns blank dib 
+MsgReqDib MsgReqDib_set(char name[NAME_LEN], void (*callback)(char*, size_t, void*), void* data); // Returns initialized dib 
+int       MsgReqDib_add(MsgReqDib element, MsgReqDib * array, size_t array_len);                  // Appends or overwrites dib into dib array
+bool      MsgReqDib_exists(char name[NAME_LEN], MsgReqDib *array, size_t array_len);              // Checks in array for preexisting dibs
+int       MsgReqDib_remove(char name[NAME_LEN], MsgReqDib *array, size_t array_len);              // Removes a dib from an array of dibs
 
 #endif 
