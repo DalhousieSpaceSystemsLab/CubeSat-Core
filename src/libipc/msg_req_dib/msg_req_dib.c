fdb9003172ae47c77ca24ab65672c137538081d1
amellalalex <github@alexamellal.com>
2020-12-19 12:38:45 -0400

    updated MsgReqDib to contain custom data pointer

--- src/libipc/msg_req_dib/msg_req_dib.c
+++ src/libipc/msg_req_dib/msg_req_dib.c
@@ -18,9 +18,10 @@ MsgReqDib MsgReqDib_new() {
 }
 
 // Returns initialized dib 
-MsgReqDib MsgReqDib_set(char name[NAME_LEN], void (*callback)(char*, size_t)) {
+MsgReqDib MsgReqDib_set(char name[NAME_LEN], void (*callback)(char*, size_t, void*), void* data) {
   MsgReqDib dib = {
-    .callback = callback
+    .callback = callback,
+    .data = data
   };
   strncpy(dib.name, name, NAME_LEN);
 
