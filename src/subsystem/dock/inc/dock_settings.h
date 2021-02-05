e5e2785e0ee7bea770ab0120b63b0556b2152872
amellalalex <github@alexamellal.com>
2021-02-05 17:10:26 -0400

    introduced dock_settings header

---
+++ src/subsystem/dock/inc/dock_settings.h
@@ -0,0 +1,14 @@
+/**
+ * settings.h
+ * 
+ * purpose: store standardized values for the subsystem module 
+ * author: alex amellal 
+ * 
+ */
+
+#ifndef CUBESAT_CORE_INCLUDE_DOCK_SETTINGS_H
+#define CUBESAT_CORE_INCLUDE_DOCK_SETTINGS_H
+
+#define MODULE_STACK_SIZE 1024*512
+
+#endif  // End of CUBESAT_CORE_INCLUDE_DOCK_SETTINGS_H guard
