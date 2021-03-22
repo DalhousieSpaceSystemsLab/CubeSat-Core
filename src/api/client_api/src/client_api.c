fc9674234eec6d11d32a881aa0f717de3b49368f
amellalalex <github@alexamellal.com>
2021-03-22 11:27:08 -0300

    modified ipc_args to prefer array for args_out

--- src/api/client_api/src/client_api.c
+++ src/api/client_api/src/client_api.c
@@ -576,7 +576,7 @@ int ipc_refresh_src(char src[NAME_LEN]) {
 }
 
 // Extracts IPC message command and arguments
-int ipc_args(char *msg, size_t msg_len, char *args_out[MAX_ARG_LEN],
+int ipc_args(char *msg, size_t msg_len, char args_out[][MAX_ARG_LEN],
              size_t max_args) {
   int argc = 0;
   int argx = 0;
