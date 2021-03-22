f9f61e80a5a546c823d6d0b264b469fa553280cc
amellalalex <github@alexamellal.com>
2021-03-22 14:07:48 -0300

    created preliminary C++ wrapper for client api with documentation

--- src/api/client_api/inc/client_api.hpp
+++ src/api/client_api/inc/client_api.hpp
@@ -1,30 +1,79 @@
+/**
+ * @file client_api.hpp
+ * @author alex amellal (loris@alexamellal.com)
+ * @brief C++ wrapper for the IPC client API
+ * @version 0.1
+ * @date 2021-03-22
+ *
+ * Dalhousie Space Systems Lab (c) 2021
+ *
+ */
+
 #ifndef __CLIENT_API_HPP__
 #define __CLIENT_API_HPP__
-#ifdef __cplusplus
 
-// Standard C++ Libraries
-#include <map>
+// Project headers
+#include "client_api.h"
+
+// Standard C++ libraries
 #include <string>
 
 // Standard C++ classes
-using std::exception;
-using std::pair;
+using std::string;
 
-// Client API (C++)
-namespace dss
-{
-namespace ipc
-{
-// Methods
-void        init(std::string name);
-void        send(std::string dest, std::string msg);
-void        send_map(const char *dest, std::map<std::string, std::string> *map);
-void        send_map(const char *dest, std::map<std::string, int> *map);
-void        send_map(const char *dest, std::map<int, int> *map);
-std::string recv(std::string src);
-void        close();
-}; // namespace ipc
-}; // namespace dss
+// Dalhousie Namespace
+namespace dss {
 
-#endif /* #ifdef __cplusplus */
+// IPC namespace
+namespace ipc {
+/**
+ * @brief Connect to IPC daemon. Must be run before any IPC operations will
+ * work.
+ *
+ * @param name 3 letter name used to register on the IPC network
+ */
+void connect(string name);
+
+/**
+ * @brief Sends string to destination process.
+ *
+ * @param dest 3 letter name belonging to destination process
+ * @param msg String to send to dest.
+ */
+void send(string dest, string msg);
+
+/**
+ * @brief Receive incoming message from IPC.
+ * **NOTE** this is a blocking type method; meaning it will not return until a
+ * message has been received.
+ *
+ * @param src	Incoming message source filter. Use "*" for wildcard (any
+ * source).
+ * @return string containing incoming message
+ */
+string recv(string src);
+
+namespace async {
+/**
+ * @brief Creates asynchronous background listener for incoming messages (with
+ * callback).
+ *
+ * @param src	Incoming message source to monitor ("*" for wildcard)
+ * @param callback Function to pass message to for processing.
+ * @param data (Optional) Additional data for callback method.
+ */
+void recv(string src, void (*callback)(string, void*));
+void recv(string src, void (*callback)(string, void*), void* data);
+
+}  // namespace async
+
+/**
+ * @brief Gracefully disconnect from the IPC.
+ *
+ */
+void disconnect();
+
+}  // namespace ipc
+}  // namespace dss
+
 #endif /* __CLIENT_API_HPP__ */
