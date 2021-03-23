7594565ec894d1a57bae1e849d586e5f1a1c6b06
amellalalex <github@alexamellal.com>
2021-03-23 17:54:31 -0300

    updated C++ wrappers to throw custom exceptions

--- src/api/client_api/src/client_api.cpp
+++ src/api/client_api/src/client_api.cpp
@@ -21,23 +21,23 @@ using std::endl;
 // Connect to IPC daemon. Must be run before any IPC operations will work.
 void dss::ipc::connect(string name) {
   if (ipc_connect(name.c_str()) < 0) {
-    throw dss::ipc::exception::IPCAPI();
+    throw dss::ipc::exceptions::IPCAPI();
   }
 }
 
 // Sends string to destination process.
 void dss::ipc::send(string dest, string msg) {
   if (dest.size() != NAME_LEN) {
-    throw dss::ipc::exception::InvalidDest();
+    throw dss::ipc::exceptions::InvalidDest();
   }
 
   if (msg.size() >= MAX_MSG_LEN) {
-    throw dss::ipc::exception::InvalidMsg();
+    throw dss::ipc::exceptions::InvalidMsg();
   }
 
   if (ipc_send((char*)dest.c_str(), (char*)msg.c_str(), msg.size()) < 0) {
     cout << "Failed to send message to " << dest << endl;
-    throw dss::ipc::exception::IPCAPI();
+    throw dss::ipc::exceptions::IPCAPI();
   }
 }
 
@@ -46,13 +46,13 @@ string dss::ipc::recv(string src) {
   if (src.size() != NAME_LEN && src != "*") {
     cout << "Destination name must be " << NAME_LEN << "characters long"
          << endl;
-    throw dss::ipc::exception::InvalidSrc();
+    throw dss::ipc::exceptions::InvalidSrc();
   }
 
   char msg[MAX_MSG_LEN];
   if (ipc_recv((char*)src.c_str(), msg, MAX_MSG_LEN) <= 0) {
     cout << "Failed to receive incoming message from IPC" << endl;
-    throw dss::ipc::exception::IPCAPI();
+    throw dss::ipc::exceptions::IPCAPI();
   }
 
   return string(msg);
