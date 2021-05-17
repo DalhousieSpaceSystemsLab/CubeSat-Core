da548e40492e807b50a9e48ac30a51bd3ded57c3
amellalalex <github@alexamellal.com>
2021-05-17 12:37:50 -0300

    added testing to build script & updated .gitignore

--- build.sh
+++ build.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-PARENT_BUILD_DIR="build"
+PARENT_BUILD_DIR="bin"
 PC_BUILD_DIR="x86"
 ARM_BUILD_DIR="arm"
 CLEAN_COMMAND="clean"
+TEST_COMMAND="test"
+RUN_TEST_COMMAND="run_test"
+
+CONFIG_PC() {
+  [ ! -d "$PC_BUILD_DIR" ] && cmake src -B $PARENT_BUILD_DIR/$PC_BUILD_DIR
+}
+
+CONFIG_ARM() {
+  [ ! -d "$ARM_BUILD_DIR" ] && cmake -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake src -B $PARENT_BUILD_DIR/$ARM_BUILD_DIR
+}
+
+CONFIG_TEST() {
+  cmake -DBUILD_TESTING=ON src -B $PARENT_BUILD_DIR/$PC_BUILD_DIR
+  cmake -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -DBUILD_TESTING=ON src -B $PARENT_BUILD_DIR/$ARM_BUILD_DIR
+}
 
 BUILD_PC() {
-  [ ! -d "$PC_BUILD_DIR" ] && cmake src -B $PARENT_BUILD_DIR/$PC_BUILD_DIR
   cmake --build $PARENT_BUILD_DIR/$PC_BUILD_DIR
 }
 
 BUILD_ARM() {
-  [ ! -d "$ARM_BUILD_DIR" ] && cmake -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake src -B $PARENT_BUILD_DIR/$ARM_BUILD_DIR
   cmake --build $PARENT_BUILD_DIR/$ARM_BUILD_DIR
 }
 
@@ -20,23 +33,32 @@ BUILD_BOTH() {
   BUILD_ARM
 }
 
+# Current only testing x86 version
+RUN_TEST() {
+  ctest --test-dir $PARENT_BUILD_DIR/$PC_BUILD_DIR
+}
+
 # Check number of arguments
 if [ $# -eq 0 ]; then
   # Config and build both
+  CONFIG_PC && BUILD_PC
+  CONFIG_ARM && BUILD_ARM
+elif [ "$1" == "$PC_BUILD_DIR" ]; then
+  CONFIG_PC && BUILD_PC
+elif [ "$1" == "$ARM_BUILD_DIR" ]; then
+  # Setup cmake (arm32)
+  CONFIG_ARM && BUILD_ARM
+elif [ "$1" == "$CLEAN_COMMAND" ]; then
+  # Delete both
+  [ -d "$PARENT_BUILD_DIR" ] && rm -r $PARENT_BUILD_DIR
+elif [ "$1" == "$TEST_COMMAND" ]; then
+  CONFIG_TEST
   BUILD_BOTH
-elif [ $# -eq 1 ]; then
-  if [ "$1" == "$PC_BUILD_DIR" ]; then
-    # Setup cmake (default)
-    BUILD_PC
-  elif [ "$1" == "$ARM_BUILD_DIR" ]; then
-    # Setup cmake (arm32)
-    BUILD_ARM
-  elif [ "$1" == "$CLEAN_COMMAND" ]; then
-    # Delete both
-    [ -d "$PARENT_BUILD_DIR" ] && rm -r $PARENT_BUILD_DIR
-  else
-    echo "$1 is not a valid option"
-  fi
+elif [ "$1" == "$RUN_TEST_COMMAND" ]; then
+  # Note: currently only testing x86 version
+  CONFIG_TEST
+  BUILD_PC
+  RUN_TEST
 else
-  echo "invalid number of arguments"
+  echo "$1 is not a valid option"
 fi
