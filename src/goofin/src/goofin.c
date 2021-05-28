0dbf0053e88a520d420d1087b9e920c5e2332cfc
amellalalex <github@alexamellal.com>
2021-05-28 10:57:53 -0300

    added goofin executable

---
+++ src/goofin/src/goofin.c
@@ -0,0 +1,12 @@
+/**
+ * @file goofin.c
+ * @author Alex Amellal (loris@alexamellal.com)
+ * @brief Coding sandbox for this repo; just for goofin & testing
+ * @version 0.1
+ * @date 2021-05-28
+ *
+ * @copyright Dalhousie Space Systems Lab (c) 2021
+ *
+ */
+
+int main() { return 0; }
