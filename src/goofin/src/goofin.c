6787fe18b88fd3fe5e0f1d9b9d0115ea48f398ac
amellalalex <github@alexamellal.com>
2021-06-01 11:22:53 -0300

    cleaned up goofin

--- src/goofin/src/goofin.c
+++ src/goofin/src/goofin.c
@@ -12,17 +12,4 @@
 #include <stdio.h>
 #include "subsysmod.h"
 
-int main() {
-  const char* raw_msg = "0x13248972 gps -12.234876 3.1415926535";
-  char args[5][MAX_ARG_LEN];
-  int n_args;
-  OK((n_args = ipc_args(raw_msg, strlen(raw_msg), args, 5)));
-
-  printf("n_args = %d\n", n_args);
-
-  for (int x = 0; x < n_args; x++) {
-    printf("[arg%d] %s\n", x, args[x]);
-  }
-
-  return 0;
-}
+int main() { return 0; }
