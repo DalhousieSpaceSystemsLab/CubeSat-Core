a88051029fded7a9871bc2085cad77ade981b915
amellalalex <github@alexamellal.com>
2021-06-01 11:01:01 -0300

    lowered max arg length

--- src/api/libipc/inc/ipc_settings.h
+++ src/api/libipc/inc/ipc_settings.h
@@ -20,7 +20,7 @@ extern "C" {
 #define MAX_NUM_CLI 16
 #define MAX_MSG_LEN 128
 #define NAME_LEN 3
-#define MAX_ARG_LEN MAX_MSG_LEN - NAME_LEN - 1
+#define MAX_ARG_LEN 32
 #define READ_BLOCK_DELAY \
   (struct timespec) { .tv_sec = 0, .tv_nsec = 100000000 }
 #define ROUTER_CHECK_DELAY 1000000  // in nanoseconds
