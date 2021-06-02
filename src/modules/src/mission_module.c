827cb631a30ed68a0fe33f2984c568db55338e61
amellalalex <github@alexamellal.com>
2021-06-02 18:32:31 -0300

    reprogrammed the mission module to listen for commands

--- src/modules/src/mission_module.c
+++ src/modules/src/mission_module.c
@@ -3,49 +3,26 @@
 // Define GPS coordinate range
 float range[4] = {10.0f, 10.0f, 45.0f, 45.0f};
 
-CALLBACK(mission_general) {
-  modprintf("incoming message: %.*s\n", msg_len, msg);
+// Handle incoming messages from command module
+CALLBACK(command) {
+  // Get args
+  int argc = ipc_get_n_args(msg, msg_len);
+  char args[argc][MAX_ARG_LEN];
+  OK(ipc_get_args(msg, msg_len, args, argc));
 
-  // done
-  STOP_CALLBACK;
-}
-
-START_MODULE(mission) {
-  OK(ipc_connect(ipc.core.msn.name))
-
-  OK(ipc_qrecv("*", mission_general, NULL, IPC_QRECV_MSG))
-
-  // Send payload the command every 5 seconds
-  for (;;) {
-    // Get GPS coordinate
-    OK(ipc_send_cmd(ipc.gps.name, ipc.gps.cmd.get_cur_pos));
-
-    // Read GPS coordinate with timeout
-    char gps_coor[MAX_MSG_LEN];
-    int bytes_read = 0;
-    IF_TIMEOUT(bytes_read =
-                   ipc_recv(ipc.gps.name, gps_coor, MAX_MSG_LEN, RECV_TIMEOUT),
-               continue);
-    OK(bytes_read);
-    gps_coor[bytes_read] = '\0';
-
-    // DEBUG
-    if (strlen(gps_coor) > 0) printf("%s\n", gps_coor);
-
-    // Parse coordinates
-    char lat_str[32], long_str[32];
-    float lattitude = 0, longitude = 0;
-    for (int x = 0; x < strlen(gps_coor); x++) {
-      if (gps_coor[x] == ',') {
-        lat_str[x] = '\0';
-        strcpy(long_str, &gps_coor[x + 1]);
-        break;
-      } else {
-        lat_str[x] = gps_coor[x];
-      }
+  // Check command -- Queue mission with GPS conditions
+  if (ipc_check_cmd(msg, "%s %s", ipc.core.msn.cmd.qmsn, "gps")) {
+    // Check argc
+    if (argc != 4) {
+      moderr(
+          "Invalid number of arguments for qmsn with gps coordinates. "
+          "SKIPPING\n");
+      STOP_CALLBACK;
     }
-    lattitude = atof(lat_str);
-    longitude = atof(long_str);
+
+    // Get lattitude and longitude
+    float lattitude = atof(args[2]);
+    float longitude = atof(args[3]);
 
     // Check if in range
     if (range[0] <= lattitude && lattitude <= range[2] &&
@@ -55,12 +32,29 @@ START_MODULE(mission) {
       // Send message to payload
       OK(ipc_send_cmd(ipc.pay.name, ipc.pay.cmd.take_pic));
     }
+  } else {
+    modprintf("wah wah didnt match\n");
+  }
 
-    // DEBUG send message to command module
-    OK(ipc_send_cmd(ipc.core.cmd.name, "%s %d", ipc.core.cmd.cmd.take_picture,
-                    10));
+  STOP_CALLBACK;
+}
 
-    // Refresh background listeners
+// Handle general incoming messages
+CALLBACK(general) {
+  modprintf("Incoming msg: %s\n", msg);
+
+  STOP_CALLBACK;
+}
+
+START_MODULE(mission) {
+  OK(ipc_connect(ipc.core.msn.name))
+
+  // Set up listeners
+  OK(ipc_create_listener(ipc.core.cmd.name, command, NULL));
+  OK(ipc_create_listener("*", general, NULL));
+
+  // Refresh incoming messages
+  for (;;) {
     OK(ipc_refresh());
   }
 }
