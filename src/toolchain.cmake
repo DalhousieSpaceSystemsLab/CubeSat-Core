16afedaaaffb282ec4b1a188f9f5f8cf684c8d5d
amellalalex <github@alexamellal.com>
2021-06-11 12:37:49 -0300

    fixed cmake compiling issue

--- src/toolchain.cmake
+++ src/toolchain.cmake
@@ -1,6 +1,9 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
+# set(CMAKE_SYSROOT /usr/local/lib/arm-none-linux-gnueabihf/arm-none-linux-gnueabihf)
+
+set(tools /usr/local/lib/arm-none-linux-gnueabihf)
 set(CMAKE_C_COMPILER arm-none-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)
 
@@ -8,3 +11,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
