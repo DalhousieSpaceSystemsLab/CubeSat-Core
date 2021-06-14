4f72a06ab2b079b910abb0890be952473411ad88
amellalalex <github@alexamellal.com>
2021-06-14 20:26:03 -0300

    modified testing GPS coordinates range

--- src/modules/src/rf_module.c
+++ src/modules/src/rf_module.c
@@ -14,13 +14,13 @@
 START_MODULE(rf) {
   OK(ipc_connect(ipc.trx.name));
 
-  // Send command to command module every second
+  // Send command to command module every 60 seconds
   for (;;) {
     OK(ipc_send_cmd(ipc.core.cmd.name, "%s %s %f %f %f %f",
-                    ipc.core.cmd.cmd.take_picture, "gps", 15.345845f,
-                    20.458347f, 25.34903f, 35.4393403f));
+                    ipc.core.cmd.cmd.take_picture, "gps", 0.0f, 0.0f, 60.0f,
+                    60.0f));
     OK(ipc_refresh());
-    sleep(1);
+    sleep(60);
   }
 }
 
