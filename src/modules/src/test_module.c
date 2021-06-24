cedd475c9b97a6c3b7ff424f0f9058ee98b24896
Iftekhar <mdiftekharhr@gmail.com>
2021-06-24 10:38:53 -0300

    Addition of doxygen to test module

--- src/modules/src/test_module.c
+++ src/modules/src/test_module.c
@@ -1,3 +1,14 @@
+/**
+ * @file test_module.c
+ * @author Iftekhar Hossain (mdiftekharhr@gmail.com)
+ * @brief Test module
+ * @version 0.1
+ * @date 2021-06-24
+ * 
+ * @copyright Dalhousie Space Systems Lab (c) 2021
+ * 
+ */
+
 #include "test_module.h"
 
 START_MODULE(test_module) {
