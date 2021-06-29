41984145d58c99a8a92d9fb72afd6daf77f41ed4
Iftekhar <mdiftekharhr@gmail.com>
2021-06-29 06:49:18 -0300

    added second test module

--- src/modules/src/test_module.c
+++ src/modules/src/test_module.c
@@ -1,5 +1,13 @@
-/**
+/**/**
  * @file test_module.c
+ * @author Iftekhar Hossain (mdiftekharhr@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-06-29
+ * 
+ * @copyright Dalhousie Space Systems Lab (c) 2021
+ * 
+ */
  * @author Iftekhar Hossain (mdiftekharhr@gmail.com)
  * @brief Test module
  * @version 0.1
@@ -12,13 +20,17 @@
 #include "test_module.h"
 
 START_MODULE(test_module) {
-
- 
+   OK(ipc_connect("tst"));
+   //send a message
+   char msg[] = "Testing for IPC connection";
+   OK(ipc_send("gps", msg, strlen(msg)));
+   sleep(1000);
 }
  
 STOP_MODULE(test_module) {
- 
+ OK(ipc_disconnect());
+
+
 }
 
 EXPORT_MODULE(test_module);
-
