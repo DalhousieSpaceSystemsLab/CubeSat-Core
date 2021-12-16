848a6e84c5ef1d29a83d4c80c572fb00cb2c079d
amellalalex <github@alexamellal.com>
2021-12-16 09:07:07 -0400

    rf module sends time command at random delay

--- src/modules/src/rf_module.c
+++ src/modules/src/rf_module.c
@@ -17,14 +17,15 @@ START_MODULE(rf) {
   // Send command to command module every 60 seconds
   for (;;) {
     OK(ipc_send_cmd(ipc.core.cmd.name, "%s %s %f %f %f %f",
-                    ipc.core.cmd.cmd.take_picture, "gps", 0.0f, 0.0f, 60.0f,
+                    ipc.core.cmd.cmd.take_picture, "gps", 30.0f, 30.0f, 60.0f,
                     60.0f));
 
     OK(ipc_send_cmd(ipc.core.cmd.name, "%s %s %ld",
-                    ipc.core.cmd.cmd.take_picture, "time", time(NULL) + 10));
+                    ipc.core.cmd.cmd.take_picture, "time",
+                    time(NULL) + (rand() % 60)));
 
     OK(ipc_refresh());
-    sleep(60);
+    sleep(10);
   }
 }
 
