da62ba5e5d8852ee0d5231ffd3ae7e5158403a3c
amellalalex <github@alexamellal.com>
2021-12-16 10:19:21 -0400

    removed troublesome characters for ipc packets

--- src/api/libipc/src/ipc_std.c
+++ src/api/libipc/src/ipc_std.c
@@ -9,19 +9,18 @@
 #include "ipc_std.h"
 
 struct ipc ipc = {
-    .pay = {.name = "pay", .cmd = {.take_pic = "ipc://pay.take_pic"}},
+    .pay = {.name = "pay", .cmd = {.take_pic = "pay.take_pic"}},
     .trx = {.name = "trx"},
     .acs = {.name = "acs"},
     .ecs = {.name = "ecs"},
-    .gps = {.name = "gps", .cmd = {.get_cur_pos = "ipc://gps.get_cur_pos"}},
+    .gps = {.name = "gps", .cmd = {.get_cur_pos = "gps.get_cur_pos"}},
 
     .core =
         {
             .fls = {.name = "fls"},
-            .msn = {.name = "msn",
-                    .cmd = {.qmsn = "ipc://core.cmd.queue_mission"}},
+            .msn = {.name = "msn", .cmd = {.qmsn = "core.cmd.queue_mission"}},
             .cmd = {.name = "cmd",
-                    .cmd = {.take_picture = "ipc://core.cmd.take_picture"}},
+                    .cmd = {.take_picture = "core.cmd.take_picture"}},
             .log = {.name = "log"},
         },
 };
