7543ddfec3d09b45587b4c2d78d7c4ee681bae8e
kfe83 <marcokaniecki@gmail.com>
2022-02-10 14:32:47 -0400

    removed submodule payload

--- src/payload/transfer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-scp bin/capture root@71.7.234.152:/home/root/payload
-
--- src/payload/transfer_images.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scp -r root@71.7.234.152:/home/root/payload/images/* /mnt/c/Users/data_/pictures/payload/
