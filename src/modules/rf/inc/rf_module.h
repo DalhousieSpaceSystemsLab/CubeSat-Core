bc689cd163347dd5496c0e119df57965e2bc1e82
amellalalex <github@alexamellal.com>
2022-05-04 10:54:17 -0300

    added some notes of warning

--- src/modules/rf/inc/rf_module.h
+++ src/modules/rf/inc/rf_module.h
@@ -9,6 +9,17 @@
  *
  */
 
+/**
+ * NOTES:
+ *
+ * - At the moment, READ_MODE_UPTO probably only works in increments of 64 bytes
+ * (because that is the write buffer size).
+ * - Functions like listen_file() probably depend on this trait for filenames to
+ * be read before the incoming file is listen for in READ_MODE_UPTO.
+ * - IF ever the size of the write buffer is changed or if filenames surpass the
+ * buffer size, there will likely be an issue reading them.
+ */
+
 #ifndef CUBESAT_CORE_MODULES_RF_MODULE_H
 #define CUBESAT_CORE_MODULES_RF_MODULE_H
 
