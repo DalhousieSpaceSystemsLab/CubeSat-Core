99846d3543741d5e50efaccf48f025aad7cb5458
amellalalex <github@alexamellal.com>
2022-05-10 10:48:49 -0300

    created global settings header for rf

--- src/modules/rf/inc/rf_module.h
+++ src/modules/rf/inc/rf_module.h
@@ -31,35 +31,13 @@
 #define CUBESAT_CORE_MODULES_RF_MODULE_H
 
 #include "antenna.h"
+#include "rf_module_settings.h"
 #include "subsysmod.h"
 
 // Standard C libraries
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-// Settings
-#define PATH_TO_ANTENNA_DEV "/dev/colibri-uartb"
-
-// Requests
-#define REQ_BASIC_TELEMETRY "A1"
-#define REQ_LARGE_TELEMETRY "B2"
-#define REQ_DELET_TELEMETRY "C3"
-#define REQ_REBOOT_OBC "D4"
-#define REQ_RESET_COMMS "E5"
-#define REQ_ENABLE_RAVEN "F6"
-#define REQ_FWD_COMMAND "CC"
-#define REQ_LISTEN_FILE "FF"
-#define REQ_GET_FILE "FR"
-#define REQ_GET_LS "LS"
-#define REQ_TAKE_PICTURE "TP"
-
-// Files
-#define FILE_BASIC_TELEMETRY "telemetry.txt"
-#define FILE_INCOMING "incoming.txt"
-#define FILE_LS_INDEX "ls_index.txt"
-#define MAX_FILENAME_LEN 64
-#define FILE_NOT_FOUND "!!FNF!!"
 
 NEW_DOCK_MODULE(rf);
 
---
+++ src/modules/rf/inc/rf_module_settings.h
@@ -0,0 +1,40 @@
+/**
+ * @file rf_module_settings.h
+ * @author Alex Amellal (loris@alexamellal.com)
+ * @brief Global settings header for the RF module
+ * @version 0.1
+ * @date 2022-05-10
+ *
+ * @copyright Dalhousie Space Systems Lab (c) 2022
+ *
+ */
+
+#ifndef CUBESAT_CORE_MODULES_RF_INCLUDE_RF_MODULE_SETTINGS_H
+#define CUBESAT_CORE_MODULES_RF_INCLUDE_RF_MODULE_SETTINGS_H
+
+// Settings
+#define PATH_TO_ANTENNA_DEV "/dev/colibri-uartb"
+
+// Requests
+#define REQ_BASIC_TELEMETRY "A1"
+#define REQ_LARGE_TELEMETRY "B2"
+#define REQ_DELET_TELEMETRY "C3"
+#define REQ_REBOOT_OBC "D4"
+#define REQ_RESET_COMMS "E5"
+#define REQ_ENABLE_RAVEN "F6"
+#define REQ_FWD_COMMAND "CC"
+#define REQ_LISTEN_FILE "FF"
+#define REQ_GET_FILE "FR"
+#define REQ_GET_LS "LS"
+#define REQ_TAKE_PICTURE "TP"
+#define REQ_ENCODE_FILE "EF"
+#define REQ_DECODE_FILE "DF"
+
+// Files
+#define FILE_BASIC_TELEMETRY "telemetry.txt"
+#define FILE_INCOMING "incoming.txt"
+#define FILE_LS_INDEX "ls_index.txt"
+#define MAX_FILENAME_LEN 64
+#define FILE_NOT_FOUND "!!FNF!!"
+
+#endif
